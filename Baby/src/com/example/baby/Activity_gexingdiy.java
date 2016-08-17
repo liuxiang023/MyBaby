e978032be90a8366c15046dcfc35d9c4ef883efd
xiangfeng99 <561027662@qq.com>
2016-08-17 18:27:08 +0800

    123

--- Baby/src/com/example/baby/Activity_gexingdiy.java
+++ Baby/src/com/example/baby/Activity_gexingdiy.java
@@ -8,15 +8,15 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Activity_gexingdiy extends Activity{
-	private Button btn,btn2;
+	private Button btn;
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	// TODO Auto-generated method stub
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_gexindiy);
-/*	btn=(Button)findViewById(R.id.btn_back3);
-	btn2=(Button)findViewById(R.id.btn_addimg);
-	btn.setOnClickListener(onClickListener);*/
+	//btn=(Button)findViewById(R.id.btn_back3);
+	btn=(Button)findViewById(R.id.btn_addimg);
+	btn.setOnClickListener(onClickListener);
 }
 OnClickListener onClickListener=new OnClickListener() {
 	
@@ -24,12 +24,16 @@ OnClickListener onClickListener=new OnClickListener() {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch(v.getId()){
-		case R.id.btn_back3:
+		/*case R.id.btn_back3:
 			Intent intent=new Intent(Activity_gexingdiy.this,Note_Fragment.class);
 			startActivity(intent);
 			break;
 			default:
-				break;
+				break;*/
+			case R.id.btn_addimg:
+				Intent intent1=new Intent(Activity_gexingdiy.this,Xiangcehuiyi_Activity.class);
+				startActivity(intent1);
+					break;
 		}
 	}
 };
